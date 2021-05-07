08df667d4e8e55cc88a87b28768e78297d4df888
Finleif <58663789+Finleif@users.noreply.github.com>
2021-05-07 19:49:21 +0200

    ferdig med alt

--- ACIoTDevice/src/main/java/no/hvl/dat110/aciotdevice/client/RestClient.java
+++ ACIoTDevice/src/main/java/no/hvl/dat110/aciotdevice/client/RestClient.java
@@ -1,9 +1,19 @@
 package no.hvl.dat110.aciotdevice.client;
 
+
+import com.google.gson.Gson;
+import com.squareup.okhttp.*;
+
+import java.io.IOException;
+
 public class RestClient {
 
-    private static String logpath = "/accessdevice/log";
+
+    private static String logpath = "/accessdevice/log/";
     private static String codepath = "/accessdevice/code";
+    private final static OkHttpClient client = new OkHttpClient();
+    private final static Gson gson = new Gson();
+
 
     public RestClient() {
         // TODO Auto-generated constructor stub
@@ -11,16 +21,55 @@ public class RestClient {
 
     public void doPostAccessEntry(String message) {
 
-        // TODO: implement a HTTP POST on the service to post the message
+
+        AccessMessage accessMessage = new AccessMessage(message);
+
+        RequestBody requestBody = RequestBody.create(MediaType
+                .parse("application/json; charset=utf-8"), gson.toJson(accessMessage));
+
+        Request request = new Request
+                .Builder()
+                .url("http://localhost:8080" + logpath)
+                .post(requestBody)
+                .build();
+
+
+        System.out.println(request.toString());
+
+        try {
+            Response response = client.newCall(request).execute();
+
+            String responseBodyAsString = response.body().string();
+
+            System.out.println(responseBodyAsString);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
 
     }
 
+
     public AccessCode doGetAccessCode() {
 
-        AccessCode code = null;
+        Request request = new Request.Builder()
+                .url("http://localhost:8080" + codepath)
+                .get()
+                .build();
 
-        // TODO: implement a HTTP GET on the service to get current access code
+        System.out.println(request.toString());
 
-        return code;
+        try {
+            Response response = client.newCall(request).execute();
+            String responseBodyAsString = response.body().string();
+
+            System.out.println(responseBodyAsString);
+
+            return gson.fromJson(responseBodyAsString, AccessCode.class);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return null;
     }
 }
